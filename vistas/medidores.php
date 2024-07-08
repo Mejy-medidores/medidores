e7af28aa59935a8c9b059f5f2cc0480daa44652f
Jorge Raul <valenciasantosjorgeraul@gmail.com>
2024-07-08 09:21:58 -0500

    Agregación de bootstrap a medidores_PHP

--- vistas/medidores.php
+++ vistas/medidores.php
@@ -11,6 +11,8 @@ $datos = leerDatos($coleccion);
     <meta charset="UTF-8">
     <title>Medidores</title>
 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
         body {
             background-color: #b0e0e6;
@@ -34,8 +36,31 @@ $datos = leerDatos($coleccion);
 </script>
 
 </head>
-<button><a href="../vistas2/medidores_entrantes.php" class="button">Ir a los medidores de entrada</a></button>
 <body>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+            <img src="../IMG/logo2.jpg" alt="Logo" width="30" height="30" class="d-inline-block align-top">
+            MEJY || 2°do Modulo
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../vistas2/medidores_entrantes.php">Medidores de salida</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+
     <h1>Salida de: <?php echo $coleccion; ?></h1>
     <img src="../IMG/logo.png" alt="CAEV" width="500" height="160">
     <br>
@@ -82,4 +107,13 @@ $datos = leerDatos($coleccion);
     </table>
        
 </body>
+<!-- Incluir jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<!-- Incluir Popper.js -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<!-- Incluir Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
 </html>
